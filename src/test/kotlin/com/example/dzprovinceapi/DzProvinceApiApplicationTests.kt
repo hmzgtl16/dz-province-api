e7bea15b36d0bb750a4e8154c86d1e6c27477f86
GATTAL Hamza <41026775+hmzgtl16@users.noreply.github.com>
2026-09-24 17:18:00 +0100

    add ci.yml

--- src/test/kotlin/com/example/dzprovinceapi/DzProvinceApiApplicationTests.kt
+++ src/test/kotlin/com/example/dzprovinceapi/DzProvinceApiApplicationTests.kt
@@ -15,12 +15,16 @@
  */
 package com.example.dzprovinceapi
 
+import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.ActiveProfiles
 
 @SpringBootTest
+@ActiveProfiles("test")
 class DzProvinceApiApplicationTests {
     @Test
     fun contextLoads() {
+        assertTrue(true, "The application context should load successfully.")
     }
 }
